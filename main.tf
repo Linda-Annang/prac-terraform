8ef479fbd03496d7a06c067dde04a609206933f6
linda ANNANG <106196912+Linda-Annang@users.noreply.github.com>
2023-08-02 18:04:43 +0000

    Update main.tf
    added more resources

--- main.tf
+++ main.tf
@@ -6,4 +6,100 @@ resource "aws_vpc" "vpc_prac" {
     Name = "vpc_prac"
   }
 }
+resource "aws_subnet" "prac-sub-1" {
+  vpc_id     = aws_vpc.vpc_prac.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "prac-sub-1"
+  }
+}
+
+resource "aws_subnet" "prac-sub-1" {
+  vpc_id     = aws_vpc.vpc_prac.id
+  cidr_block = "10.0.1.0/26"
+
+  tags = {
+    Name = "prac-sub-1"
+  }
+}
+
+resource "aws_subnet" "prac-sub-2" {
+  vpc_id     = aws_vpc.vpc_prac.id
+  cidr_block = "10.0.1.0/26"
+
+  tags = {
+    Name = "prac-sub-2"
+  }
+}
+
+resource "aws_subnet" "prac-sub-3" {
+  vpc_id     = aws_vpc.vpc_prac.id
+  cidr_block = "10.0.1.0/26"
+
+  tags = {
+    Name = "prac-sub-3"
+  }
+}
+
+resource "aws_subnet" "prac-sub-4" {
+  vpc_id     = aws_vpc.vpc_prac.id
+  cidr_block = "10.0.1.0/26"
+
+  tags = {
+    Name = "prac-sub-4"
+  }
+}
+
+resource "aws_route_table" "public-rt" {
+  vpc_id = aws_vpc.vpc_prac.id
+
+  tags = {
+    Name = "public-rt"
+  }
+}
+
+resource "aws_route_table" "private-rt" {
+  vpc_id = aws_vpc.vpc_prac.id
+
+  tags = {
+    Name = "private-rt"
+  }
+}
+
+resource "aws_internet_gateway" "prac-igw" {
+  vpc_id = aws_vpc.vpc_prac.id
+
+  tags = {
+    Name = "vpc_prac"
+  }
+}
+
+
+resource "aws_route_table_association" "public-rta-a" {
+  subnet_id      = aws_subnet.prac-sub-1.id
+  route_table_id = aws_route_table.public-rt.id
+}
+
+resource "aws_route_table_association" "public-rta-b" {
+  subnet_id      = aws_subnet.prac-sub-2.id
+  route_table_id = aws_route_table.public-rt.id
+}
+
+resource "aws_route_table_association" "private-rta-a" {
+  subnet_id      = aws_subnet.prac-sub-3.id
+  route_table_id = aws_route_table.private-rt.id
+}
+
+resource "aws_route_table_association" "private-rta-b" {
+  subnet_id      = aws_subnet.prac-sub-4.id
+  route_table_id = aws_route_table.private-rt.id
+}
+
+resource "aws_route" "public-igw-route" {
+  route_table_id            = aws_route_table.public-rt.id
+  destination_cidr_block    = "0.0.0.0/0"
+  gateway_id     = aws_internet_gateway.prac-igw.id
+}
+
 
